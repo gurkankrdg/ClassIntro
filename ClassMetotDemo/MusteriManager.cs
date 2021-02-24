3bbd899891076e6377ba84624baef343af48dfca
gürkan karadağ <gurkankaradag37@gmail.com>
2021-02-24 15:59:39 +0300

    ClassMetotDemo Ödevi Dahil Edilmiş

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void musteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen Müşteri Kimlik No : " + musteri.Id);
+            Console.WriteLine("Eklenen Müşteri Adı : " + musteri.Ad);
+            Console.WriteLine("Eklenen Müşteri Soyadı : " + musteri.Soyad);
+        }
+        public void musteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Silinen Müşteri Kimlik No : " + musteri.Id);
+            Console.WriteLine("Silinen Müşteri Adı : " + musteri.Ad);
+            Console.WriteLine("Silinen Müşteri Soyadı : " + musteri.Soyad);
+        }
+        public void musteriBilgiListeleme(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Listlenen Müşteri Kimlik No : " + musteri.Id);
+                Console.WriteLine("Listelenen Müşteri Adı : " + musteri.Ad);
+                Console.WriteLine("Listelenen Müşteri Soyadı : " + musteri.Soyad);
+            }
+            
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1234566789;
+            musteri1.Ad = "Hasan";
+            musteri1.Soyad = "Kartal";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 232323451;
+            musteri2.Ad = "Sedat";
+            musteri2.Soyad = "Karman";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.musteriEkleme(musteri1);
+            musteriManager.musteriSilme(musteri2);
+            musteriManager.musteriBilgiListeleme(musteriler);
+        }
+    }
+}
